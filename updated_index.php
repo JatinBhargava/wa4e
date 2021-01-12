954de5fdceada8d1ac5b04f51e05009785f9b9c9
JatinBhargava <63741896+JatinBhargava@users.noreply.github.com>
2021-01-12 20:38:31 +0530

    Create updated_index.php

---
+++ updated_index.php
@@ -0,0 +1,11 @@
+          }
+        }
+    }
+    // Compute ellapsed time
+    // Compute elapsed time
+    $time_post = microtime(true);
+    print "Ellapsed time: ";
+    print "Elapsed time: ";
+    print $time_post-$time_pre;
+    print "\n";
+}
